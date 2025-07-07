aa0a6e3ceba2b31c8d947caad6885c5883a14d84
Ada Martin <adamartin@MacBook-Air-3.local>
2025-07-07 15:41:56 -0400

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+cJSON.c: https://github.com/DaveGamble/cJSON (MIT license)
+sm83: https://github.com/SingleStepTests/sm83
+
+to build with debug flags: gcc main.c cJSON.c -g -o main
---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include "cJSON.h"
+
+int main(){
+
+    char* buffer = malloc(1 << 20);
+    struct dirent *dir;
+    DIR *d;
+    d = opendir("./sm83/v1/"); // Open the current directory
+    char* filenames[1024];
+    int n_files = 0;
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if(dir->d_name[0] == '.') continue; // exclude . and ..
+            filenames[n_files] = strdup(dir->d_name);
+            n_files += 1;
+        }
+        closedir(d);
+    } else {
+        printf("No test JSONs found!");
+    }
+
+    // for(int file_idx = 0; file_idx < n_files; file_idx++){
+    //     char* file_name = strcat("./sm83/v1", )
+    // }
+
+    FILE *fp = fopen("asdf.c8", "rb");
+    int bytesRead = fread(buffer, 1, 1<<20, fp);
+    printf("%x%x\n", buffer[0], buffer[1]);
+    return 0;
+}

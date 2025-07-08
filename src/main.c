4187d4ab4f3af51bcdfbe781b43673c5bdc225c4
niftyborg <nb@niftyborg.xyz>
2025-07-07 21:40:12 -0400

    Factor out dir traversal, move allocs to heap
    Factor out directory traversal to separate function
    Move allocations to heap. I have found that we are less likely to hit
    stack bugs when doing large allocations on the heap rather then the stack.
    Free'd memory, not strickly necessary since it's in the main function
    but easier to find other leaks with valgrind this way.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,24 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
-#include "cJSON.h"
 
-int main(void) {
+#include <cJSON.h>
 
-    char* buffer = malloc(1 << 20);
-    struct dirent *dir;
-    DIR *d;
-    d = opendir("./sm83/v1/"); // Open the current directory
-    char* filenames[1024];
+#define SM83_DIR "./sm83/v1/"
+
+int cmp_str (const void *a, const void *b) {
+    return strcmp(*(char**)a, *(char**)b);
+}
+
+int test_file_list(char **filenames, size_t *filecount) {
+    int error = 0;
+    struct dirent *dir = {0};
+    DIR *d = {0};
+    assert (filenames != NULL && "Failed to alloc filenames");
+
+    d = opendir(SM83_DIR); // Open the current directory
     int n_files = 0;
     if (d) {
         while ((dir = readdir(d)) != NULL) {
@@ -18,10 +26,29 @@ int main(void) {
             filenames[n_files] = strdup(dir->d_name);
             n_files += 1;
         }
-        closedir(d);
+        *filecount = n_files;
     } else {
-        printf("No test JSONs found!");
+        printf("Failed to read %s\n", SM83_DIR);
+        error = 1;
     }
+    if (d) closedir(d);
+    return error;
+}
+
+int main(void) {
+
+    char* buffer = malloc(1 << 20);
+    if (buffer == NULL)
+        return EXIT_FAILURE;
+
+    char **filenames = calloc(1024, sizeof(char*));
+    size_t filenames_count = 0;
+    test_file_list (filenames, &filenames_count);
+    qsort(filenames, filenames_count, sizeof(char*), cmp_str);
+
+    printf("Test Files\n");
+    for (size_t i = 0; i < filenames_count; i++)
+        printf("%zu: %s\n", i, filenames[i]);
 
     // for(int file_idx = 0; file_idx < n_files; file_idx++){
     //     char* file_name = strcat("./sm83/v1", )
@@ -31,5 +58,10 @@ int main(void) {
     // FILE *fp = fopen("asdf.c8", "rb");
     // int bytesRead = fread(buffer, 1, 1<<20, fp);
     // printf("%x%x\n", buffer[0], buffer[1]);
+    if (buffer) free(buffer);
+    if (filenames) {
+        for (size_t i = 0; i < filenames_count; i++) free(filenames[i]);
+        free (filenames);
+    }
     return 0;
 }
